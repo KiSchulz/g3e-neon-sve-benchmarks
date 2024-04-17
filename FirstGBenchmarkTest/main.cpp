5ac14429b92324c43e094acdd7322deeb31bd4fd
Kilian Schulz <ge64zuw@mytum.de>
2024-04-17 16:23:02 +0200

    worked on benchmarking setup

--- FirstGBenchmarkTest/main.cpp
+++ FirstGBenchmarkTest/main.cpp
@@ -1,7 +1,7 @@
 #include <benchmark/benchmark.h>
-#include <unistd.h>
 #include <cstdint>
 #include <iostream>
+#include <unistd.h>
 
 #ifndef __ARM_FEATURE_SVE
 #define __ARM_FREATURE_SVE
@@ -9,30 +9,41 @@
 
 #include <arm_sve.h>
 
-template<class T = float>
-void print_svfloat32_t(std::string_view name, svbool_t pred, svfloat32_t m) {
-    const uint64_t n = svcntp_b32(pred, pred);
+// template<class T = float>
+// void print_svfloat32_t(std::string_view name, svbool_t pred, svfloat32_t m) {
+//     const uint64_t n = svcntp_b32(pred, pred);
+//
+// }
+//
+// int main () {
+//     std::cout << svcntb() << "\n";
+// }
 
+uint64_t someCoolFunc() {
+  uint64_t n = 0;
+  for (int i = 0; i < 100; i++) {
+    n += getpid();
+  }
+  return n;
 }
 
-int main () {
-    std::cout << svcntb() << "\n";
+static void bm_func(benchmark::State &state) {
+  for ([[maybe_unused]] auto _ : state) {
+    someCoolFunc();
+  }
 }
 
-//uint64_t someCoolFunc() {
-//    uint64_t n = 0;
-//    for (int i = 0; i < 100; i++) {
-//        n += getpid();
-//    }
-//    return n;
-//}
-//
-//static void bm_func(benchmark::State &state) {
-//    for ([[maybe_unused]] auto _: state) {
-//        someCoolFunc();
-//    }
-//}
-//
-//BENCHMARK(bm_func)->Repetitions(10)->ReportAggregatesOnly(true);
-//
-//BENCHMARK_MAIN();
+BENCHMARK(bm_func)->Repetitions(10)->ReportAggregatesOnly(true);
+
+int main(int argc, char **argv) {
+  if (argc == 1) {
+    std::vector<char *> defaultArgv = {argv[0], "--benchmark_counters_tabular=true",
+                                       "--benchmark_perf_counters=CYCLES,INSTRUCTIONS"};
+    int defaultArgc = (int)defaultArgv.size();
+    benchmark::Initialize(&defaultArgc, &defaultArgv[0]);
+  } else {
+    benchmark::Initialize(&argc, argv);
+  }
+  benchmark::RunSpecifiedBenchmarks();
+  benchmark::Shutdown();
+}
