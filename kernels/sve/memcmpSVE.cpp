730420cc02bcd068fa77d5d9612aad7f3865bfb5
Kilian Schulz <ge64zuw@mytum.de>
2024-04-30 20:49:30 +0200

    looked into incb (turned out to be slow) and optimized loop via inline asm

--- kernels/sve/memcmpSVE.cpp
+++ kernels/sve/memcmpSVE.cpp
@@ -7,9 +7,9 @@ int sve_kernels::memcmp(const void *in_lhs, const void *in_rhs, std::size_t coun
 
   const auto *lhs_end = lhs + count;
 
-  for (; lhs < lhs_end; lhs += vl, rhs += vl) {
+  svbool_t pred = svwhilelt_b8_u64((uint64_t)lhs, (uint64_t)lhs_end);
+  while (lhs < lhs_end) {
     // initializing pred with a fitting length
-    const svbool_t pred = svwhilelt_b8_u64((uint64_t)lhs, (uint64_t)lhs_end);
     // load data
     const svuint8_t l = svldff1_u8(pred, lhs);
     const svuint8_t r = svldff1_u8(pred, rhs);
@@ -25,7 +25,34 @@ int sve_kernels::memcmp(const void *in_lhs, const void *in_rhs, std::size_t coun
       const int r_ne = svlastb_u8(first_ne_mask, r);
       return l_ne - r_ne;
     }
+    lhs += vl;
+    rhs += vl;
+    pred = svwhilelt_b8_u64((uint64_t)lhs, (uint64_t)lhs_end);
   }
 
   return 0;
+
+  //__asm__("cmp x2, #0x1\n"
+  //        "b.lt _rz\n"
+  //        "add x8, x0, x2\n"
+  //        "whilelo p0.b, x0, x8\n"
+  //        "_l1:\n"
+  //        "ldff1b {z1.b}, p0/z, [x0, xzr]\n"
+  //        "ldff1b {z0.b}, p0/z, [x1, xzr]\n"
+  //        "cmpne p1.b, p0/z, z1.b, z0.b\n"
+  //        "b.ne _rne\n"
+  //        "add x0, x0, #0x20\n"
+  //        "add x1, x1, #0x20\n"
+  //        "whilelo p0.b, x0, x8\n"
+  //        "b.first _l1\n"
+  //        "_rz:\n"
+  //        "mov w0, wzr\n"
+  //        "ret\n"
+  //        "_rne:\n"
+  //        "brka p0.b, p0/z, p1.b\n"
+  //        "lastb w8, p0, z1.b\n"
+  //        "lastb w9, p0, z0.b\n"
+  //        "and w8, w8, #0xff\n"
+  //        "sub w0, w8, w9, uxtb\n"
+  //        "ret\n");
 }
