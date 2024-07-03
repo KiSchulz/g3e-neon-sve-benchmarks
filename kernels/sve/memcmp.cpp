37927a980bfdb39b7f2cd7453334c61cce6a37dd
Kilian Schulz <ge64zuw@mytum.de>
2024-07-03 16:12:38 +0200

    added loop tail to sve acc

--- kernels/sve/memcmp.cpp
+++ kernels/sve/memcmp.cpp
@@ -1,59 +1,52 @@
 #include "sve_common.h"
 
- int returnHelper(svbool_t p, svuint8_t l, svuint8_t r) {
-  // set all elements to active until the first that were not equal in l0 and r0, including the not equal element
-  const svbool_t first_ne_mask = svbrka_b_z(svptrue_b8(), svcmpne(p, l, r));
-  // extract the last active element in l0 and r0 (the not equal elements)
-  const int l_ne = svlastb_u8(first_ne_mask, l);
-  const int r_ne = svlastb_u8(first_ne_mask, r);
-  return l_ne - r_ne;
-}
-
 int sve_kernels::memcmp(const void *in_lhs, const void *in_rhs, std::size_t count) {
   constexpr uint64_t n = 4;
   const uint64_t vl = svcntb();
+  const uint64_t vLen = count - (count % (vl * n));
   const auto *lhs = (const uint8_t *)in_lhs;
   const auto *rhs = (const uint8_t *)in_rhs;
 
-  const auto *lhs_end = lhs + count;
-
   svbool_t p0 = svptrue_b8();
-  svbool_t p1 = svptrue_b8();
-  svbool_t p2 = svptrue_b8();
-  for(; lhs < lhs_end; lhs += n * vl, rhs += n * vl) {
-    // Todo create loop tail
-    svbool_t pl = svwhilelt_b8_u64((uint64_t)lhs + 3 * vl, (uint64_t)lhs_end);
-    if (lhs + n * vl >= lhs_end) [[unlikely]] {
-      p0 = svwhilelt_b8_u64((uint64_t)lhs + 0 * vl, (uint64_t)lhs_end);
-      p1 = svwhilelt_b8_u64((uint64_t)lhs + 1 * vl, (uint64_t)lhs_end);
-      p2 = svwhilelt_b8_u64((uint64_t)lhs + 2 * vl, (uint64_t)lhs_end);
-    }
+  for (; lhs < (const uint8_t*)in_lhs + vLen; lhs += n * vl, rhs += n * vl) {
     // load data
     svuint8_t l0 = svld1_u8(p0, lhs + 0 * vl);
-    svuint8_t l1 = svld1_u8(p1, lhs + 1 * vl);
-    svuint8_t l2 = svld1_u8(p2, lhs + 2 * vl);
-    svuint8_t ll = svld1_u8(pl, lhs + 3 * vl);
+    svuint8_t l1 = svld1_u8(p0, lhs + 1 * vl);
+    svuint8_t l2 = svld1_u8(p0, lhs + 2 * vl);
+    svuint8_t ll = svld1_u8(p0, lhs + 3 * vl);
 
     svuint8_t r0 = svld1_u8(p0, rhs + 0 * vl);
     svuint8_t r1 = svld1_u8(p0, rhs + 1 * vl);
     svuint8_t r2 = svld1_u8(p0, rhs + 2 * vl);
-    svuint8_t rl = svld1_u8(pl, rhs + 3 * vl);
+    svuint8_t rl = svld1_u8(p0, rhs + 3 * vl);
 
     svuint8_t e0 = sveor_x(p0, l0, r0);
-    svuint8_t e1 = sveor_x(p1, l1, r1);
-    svuint8_t e2 = sveor_x(p2, l2, r2);
-    svuint8_t el = sveor_x(pl, ll, rl);
+    svuint8_t e1 = sveor_x(p0, l1, r1);
+    svuint8_t e2 = sveor_x(p0, l2, r2);
+    svuint8_t el = sveor_x(p0, ll, rl);
 
-    svuint8_t et0 = sveor_x(svptrue_b8(), e0, e1);
-    svuint8_t et1 = sveor_x(svptrue_b8(), e2, el);
-    et0 = sveor_x(svptrue_b8(), et0, et1);
+    svuint8_t et0 = sveor_x(p0, e0, e1);
+    svuint8_t et1 = sveor_x(p0, e2, el);
+    et0 = sveor_x(p0, et0, et1);
 
     // test if any active element was not equal in l0 and r0
-    if (svmaxv(svptrue_b8(), et0)) [[unlikely]] {
-      if (svmaxv(svptrue_b8(), e0)) return returnHelper(p0, l0, r0);
-      if (svmaxv(svptrue_b8(), e1)) return returnHelper(p1, l1, r1);
-      if (svmaxv(svptrue_b8(), e2)) return returnHelper(p2, l2, r2);
-      if (svmaxv(svptrue_b8(), el)) return returnHelper(pl, ll, rl);
+    if (svmaxv(p0, et0)) [[unlikely]] {
+      break;
+    }
+  }
+
+  for (; lhs < (const uint8_t*)in_lhs + count; lhs += vl, rhs += vl) {
+    p0 = svwhilelt_b8_u64((uint64_t)lhs, (uint64_t)in_lhs + count);
+    svuint8_t l = svld1(p0, lhs);
+    svuint8_t r = svld1(p0, rhs);
+    svuint8_t e = sveor_x(p0, l, r);
+    if (svmaxv(p0, e)) {
+      // set all elements to active until the first that were not equal in l0 and r0, including the not equal element
+      const svbool_t firstNeMask = svbrka_b_z(svptrue_b8(), svcmpne(p0, l, r));
+      // extract the last active element in l0 and r0 (the not equal elements)
+      const int lNe = svlastb_u8(firstNeMask, l);
+      const int rNe = svlastb_u8(firstNeMask, r);
+      return lNe - rNe;
     }
   }
 
