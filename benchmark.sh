d4e15eea29387382c7312fc9340a6c330716065d
Kilian Schulz <ge64zuw@mytum.de>
2024-07-04 11:53:22 +0200

    added benchmark script

---
+++ benchmark.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cpu=$(sudo lshw -C CPU | grep 'product') && cpu=${cpu#*: } && cpu=${cpu// /_} && cpu=$(sed 's/[)(]//g' <<< $cpu)
+filename="${cpu}.json"
+
+mkdir -p build
+cd build || exit
+rm -rf ./*
+
+cmake -E env CXX=clang++ CC=clang cmake -GNinja ..
+ninja test_driver benchmark_driver
+test_driver/test_driver --gtest_brief=1
+benchmark_driver/benchmark_driver -def --benchmark_format=json --benchmark_out="$filename" --benchmark_context=CPU="$cpu"
+
+cp "$filename" "../../Thesis/data/"
