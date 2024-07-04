5b1e37cd5084631abb0bc6159f07a8ff41f7f933
Kilian Schulz <ge64zuw@mytum.de>
2024-07-04 18:56:09 +0200

    fixed counters for multi threading

--- benchmark_driver/benchmark_common.h
+++ benchmark_driver/benchmark_common.h
@@ -11,13 +11,14 @@ namespace neon = neon_kernels;
 namespace sve = sve_kernels;
 
 void addClockCounter(benchmark::State &state) {
-  state.counters["clock"] = benchmark::Counter{state.counters["CYCLES"], benchmark::Counter::kIsRate};
+  state.counters["clock"] =
+      benchmark::Counter{state.counters["CYCLES"] / (double)state.threads(), benchmark::Counter::kIsRate};
 }
 
 void addByteCounters(benchmark::State &state, uint64_t bytes_per_iter) {
   state.counters["bytes"] = (double)bytes_per_iter;
-  state.counters["bytes/s"] =
-      benchmark::Counter{(double)state.iterations() * (double)bytes_per_iter, benchmark::Counter::kIsRate};
+  state.counters["bytes/s"] = benchmark::Counter{
+      (double)state.threads() * (double)state.iterations() * (double)bytes_per_iter, benchmark::Counter::kIsRate};
 }
 
 #endif // NEON_SVE_BENCH_BENCHMARK_COMMON_H
